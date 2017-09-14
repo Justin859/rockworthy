9144ce0c15da8c4cbc9f1431beb6774cf4d464b9
Justin859 <justinhammond859@yahoo.com>
2017-09-14 12:17:31 +0200

    Made adjustments to layout.

--- rockworthy/rockworthy/static/vendor/custom.js
+++ rockworthy/rockworthy/static/vendor/custom.js
@@ -1,15 +1,19 @@
 
 $(document).ready(function(){
 
-$('.menu-pick').click(function() {
+$('.menu-pick').click(function(e) {
     if ($('#navigation-side-bar').hasClass('nav-closed')) {
         $('nav').removeClass('nav-closed');
         $('nav').addClass('nav-open');
+        $('.nav-button').removeClass('nav-button-closed')
+        $('.nav-button').addClass('nav-button-open');        
     } else {
         $('nav').removeClass("nav-open");
         $('nav').addClass("nav-closed");
+        $('.nav-button').removeClass('nav-button-open')
+        $('.nav-button').addClass('nav-button-closed');
     }
-    
+    e.preventDefault()
 });
 
 });
--- rockworthy/rockworthy/views.py
+++ rockworthy/rockworthy/views.py
@@ -10,25 +10,20 @@ import requests
 import itertools
 
 def index(request):
-    hosts = []
     events = []
-    most_popular = []
 
     event_hosts = EventHost.objects.all()
-
-    for host in event_hosts:
-        hosts.append(host.host_id)
 
     access_token = json.loads(requests.get(
         "https://graph.facebook.com/oauth/access_token?client_id=" + os.environ.get('APP_ID') + "&client_secret=" + os.environ.get('APP_SECRET')  + "&grant_type=client_credentials").content.decode('utf-8'))['access_token']
 
     def get_host_events(host_ids):
-        for id in host_ids:
+        for event in event_hosts:
             result = json.loads(requests.get(
-                "https://graph.facebook.com/" + id + "/?fields=events{cover,name,attending_count,start_time,place}&access_token=" + access_token).content.decode('utf-8'))['events']['data']
+                "https://graph.facebook.com/" + str(event.host_id) + "/?fields=events{cover,name,attending_count,start_time,place}&access_token=" + access_token).content.decode('utf-8'))['events']['data']
             events.append(result)
 
-    get_host_events(hosts)
+    get_host_events(events)
     events = list(itertools.chain.from_iterable(events))
     events_popular = sorted(events, key=lambda k: k['attending_count'], reverse=True) 
 

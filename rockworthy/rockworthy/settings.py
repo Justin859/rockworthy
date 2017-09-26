c5a648b9f02af8b0fe3932f1bf56e01f74fddeba
Justin859 <justinhammond859@yahoo.com>
2017-09-26 17:55:18 +0200

    Small adjustments to layout.

--- rockworthy/rockworthy/settings.py
+++ rockworthy/rockworthy/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(root_path('.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rockworthy-env.zdm3bhufhs.us-east-1.elasticbeanstalk.com', '127.0.0.1']
 

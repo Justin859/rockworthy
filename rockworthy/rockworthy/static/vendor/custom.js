021d1f49649493163d720ad9311f8b9c0e6c1bec
Justin859 <justinhammond859@yahoo.com>
2017-09-26 17:35:37 +0200

    Changed Imagesloaded to initializeafter.

--- rockworthy/rockworthy/static/vendor/custom.js
+++ rockworthy/rockworthy/static/vendor/custom.js
@@ -1,59 +1,63 @@
 $(document).ready(function(){    
 
-    var $grid = $('.grid').masonry({
-        // options...
-        itemSelector: '.grid-item',
-        transitionDuration: 0,
-      });
-    
-    $grid.imagesLoaded().progress(function() {
-        // init Masonry
-        $grid.masonry('layout');
-        $('.card').removeClass('hide');
-        $('.loading').addClass('hide');
-    
-        var sticky = new Waypoint.Sticky({
-            element: $('#weekend-heading')[0]
+    var $grid = $('.grid').imagesLoaded( function(){
+        // init Masonry after all images have loaded
+        $grid.masonry({
+            // options...
+            itemSelector: '.grid-item',
+            transitionDuration: 0,
         })
-
-        var sticky = new Waypoint.Sticky({
-            element: $('#upcoming-heading')[0]
-        })
-
-        $('.sticky-wrapper').addClass('hidden-md-up');
-    
-        var waypoint = new Waypoint({
-            element: document.getElementById('weekend-events-heading'),
-            handler: function(direction) {
-                if(direction === 'down') {
-                    $('#weekend-events-tag').removeClass('events-tag-closed');
-                    $('#weekend-events-tag').addClass('events-tag-open');
-                } else {
-                    $('#weekend-events-tag').addClass('events-tag-closed');                    
-                    $('#weekend-events-tag').removeClass('events-tag-open');                    
-                }
-    
-            }
-        })
-    
-        var waypoint = new Waypoint({
-            element: document.getElementById('upcoming-events-heading'),
-            handler: function(direction) {
-                if(direction === 'down') {                    
-                    $('#weekend-events-tag').addClass('events-tag-closed');
-                    $('#upcoming-events-tag').removeClass('events-tag-closed');
-                    $('#upcoming-events-tag').addClass('events-tag-open');
-                } else {
-                    $('#weekend-events-tag').removeClass('events-tag-closed');                    
-                    $('#weekend-events-tag').addClass('events-tag-open');
-                    $('#upcoming-events-tag').addClass('events-tag-closed');                    
-                    $('#upcoming-events-tag').removeClass('events-tag-open');                    
-                }
-    
-            }
-        })
+        $grid.imagesLoaded().progress(function() {
+            // init Masonry
+            $grid.masonry('layout');
+            $('.card').removeClass('hide');
+            $('.loading').addClass('hide');
         
+            var sticky = new Waypoint.Sticky({
+                element: $('#weekend-heading')[0]
+            })
+    
+            var sticky = new Waypoint.Sticky({
+                element: $('#upcoming-heading')[0]
+            })
+    
+            $('.sticky-wrapper').addClass('hidden-md-up');
+        
+            var waypoint = new Waypoint({
+                element: document.getElementById('weekend-events-heading'),
+                handler: function(direction) {
+                    if(direction === 'down') {
+                        $('#weekend-events-tag').removeClass('events-tag-closed');
+                        $('#weekend-events-tag').addClass('events-tag-open');
+                    } else {
+                        $('#weekend-events-tag').addClass('events-tag-closed');                    
+                        $('#weekend-events-tag').removeClass('events-tag-open');                    
+                    }
+        
+                }
+            })
+        
+            var waypoint = new Waypoint({
+                element: document.getElementById('upcoming-events-heading'),
+                handler: function(direction) {
+                    if(direction === 'down') {                    
+                        $('#weekend-events-tag').addClass('events-tag-closed');
+                        $('#upcoming-events-tag').removeClass('events-tag-closed');
+                        $('#upcoming-events-tag').addClass('events-tag-open');
+                    } else {
+                        $('#weekend-events-tag').removeClass('events-tag-closed');                    
+                        $('#weekend-events-tag').addClass('events-tag-open');
+                        $('#upcoming-events-tag').addClass('events-tag-closed');                    
+                        $('#upcoming-events-tag').removeClass('events-tag-open');                    
+                    }
+        
+                }
+            })
+            
+          });
+    
       });
+    
 
     $('.menu-pick').click(function(e) {
         if ($('#navigation-side-bar').hasClass('nav-closed')) {
